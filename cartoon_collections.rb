84c239fc50f7551a232ffd280c93fd1dd2385a78
Daniel Kim <daniel.kim52@gmail.com>
2020-02-29 13:41:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(elements)
   elements.map {|summon| summon.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.map {|lol| if lol.length > 4 return true else return false end}
+
 end
 
 def find_the_cheese# code an argument here
